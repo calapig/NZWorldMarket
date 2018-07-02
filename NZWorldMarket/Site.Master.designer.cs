55ce66045d5a347211f74051fdf5b4a292e325a6
JORGE EMILIO CALAPINA ROZO <joca006@ess.ais.ac.nz>
2018-07-02 12:04:52 +1200

    Logo Add, adjust of body and container  padding

--- NZWorldMarket/Site.Master.designer.cs
+++ NZWorldMarket/Site.Master.designer.cs
@@ -13,6 +13,33 @@ namespace NZWorldMarket {
     public partial class SiteMaster {
         
         /// <summary>
+        /// ddlContinent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlContinent;
+        
+        /// <summary>
+        /// dsDdlContinent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource dsDdlContinent;
+        
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
